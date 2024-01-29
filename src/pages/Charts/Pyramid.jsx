eb1149b6a89486bec6de3c010e3f6945660f0650
Emerson Viçosa <emersonvicosa@gmail.com>
2024-01-29 17:58:05 -0300

    Atualizando os dados do gráfico de pyramid com django

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, AccumulationLegend, AccumulationDataLabel, AccumulationTooltip, PyramidSeries, AccumulationSelection } from '@syncfusion/ej2-react-charts';
+import axios from 'axios';
 
-import { PyramidData } from '../../data/dummy';
 import { useStateContext } from '../../ContextProvider';
 import { ChartsHeader } from '../../components/Charts';
 
 const Pyramid = () => {
   const { currentMode } = useStateContext();
+
+  const [pyramidData, setPyramidData] = useState([]);
+
+  useEffect (() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/loja1/');
+
+        const sortedData = response.data.sort((a, b) => a.rendimento_mensal - b.rendimento_mensal);
+        setPyramidData(sortedData);
+
+        console.log('Dados do banco de dados:', sortedData);
+      } catch (error) {
+        console.error('Erro ao obter os dados: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="m-4 md:m-10 mt-24  p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
@@ -21,10 +40,10 @@ const Pyramid = () => {
           <Inject services={[AccumulationDataLabel, AccumulationTooltip, PyramidSeries, AccumulationLegend, AccumulationSelection]} />
           <AccumulationSeriesCollectionDirective>
             <AccumulationSeriesDirective
-              name="Food"
-              dataSource={PyramidData}
-              xName="x"
-              yName="y"
+              name="Loja 1"
+              dataSource={pyramidData}
+              xName="tipo_produto"
+              yName="rendimento_mensal"
               type="Pyramid"
               width="45%"
               height="80%"
