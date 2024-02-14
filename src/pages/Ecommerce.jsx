be9f6e9662372965c7a44db1967855e011f1c02b
Emerson Viçosa <emersonvicosa@gmail.com>
2024-02-13 23:38:26 -0300

    os valores funcionam, mas so carregam qndo mexe no codigo

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { GoDotFill } from 'react-icons/go';
 import { Stacked, Button, SparkLine } from '../components/Charts';
-import { earningData, SparklineAreaData } from '../data/dummy';
+import { SparklineAreaData } from '../data/dummy';
 import { useStateContext } from '../ContextProvider';
+
+import { BsBoxSeam } from 'react-icons/bs';
+import { MdOutlineSupervisorAccount } from 'react-icons/md';
+import { FiBarChart } from 'react-icons/fi';
+import { HiOutlineRefresh } from 'react-icons/hi';
 
 import axios from 'axios';
 
@@ -14,7 +19,7 @@ const Ecommerce = () => {
   const [lojaData1, setLojaData] = useState([]);
   const [lojaData2, setLojaData2] = useState([]);
   const [lojaData3, setLojaData3] = useState([]);
-    
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -25,7 +30,7 @@ const Ecommerce = () => {
         setLojaData(response1.data);
         setLojaData2(response2.data);
         setLojaData3(response3.data);
-        
+
         const loja1Variables = response1.data.map(item => ({
           rendimento_mensal: item.rendimento_mensal,
           menor_venda: item.menor_venda
@@ -38,15 +43,29 @@ const Ecommerce = () => {
           rendimento_mensal: item.rendimento_mensal,
           menor_venda: item.menor_venda
         }));
-        
+
         const allVariables = [...loja1Variables, ...loja2Variables, ...loja3Variables];
 
         const total = allVariables.reduce((acc, curr) => acc + curr.rendimento_mensal, 0);
-        setTotalEarnings(total)
-      
+        setTotalEarnings(total);
+
         const totalDeficit = allVariables.reduce((acc, curr) => acc + curr.menor_venda, 0);
-        setTotalDeficit(totalDeficit)
-        
+        setTotalDeficit(totalDeficit);
+
+        // Atualize os valores de amount em earningData com os rendimentos mensais de cada loja
+        const updatedEarningData = earningData.map((item, index) => {
+          switch (index) {
+            case 0:
+              return { ...item, amount: lojaData1.map(data => data.rendimento_mensal).reduce((acc, curr) => acc + curr, 0) };
+            case 1:
+              return { ...item, amount: lojaData2.map(data => data.rendimento_mensal).reduce((acc, curr) => acc + curr, 0) };
+            case 2:
+              return { ...item, amount: lojaData3.map(data => data.rendimento_mensal).reduce((acc, curr) => acc + curr, 0) };
+            default:
+              return item;
+          }
+        });
+        setEarningData(updatedEarningData);
 
       } catch (error) {
         console.error('Erro ao obter dados do backend:', error);
@@ -55,6 +74,46 @@ const Ecommerce = () => {
 
     fetchData();
   }, []);
+
+  const [earningData, setEarningData] = useState([
+    {
+      icon: <MdOutlineSupervisorAccount />,
+      amount: '',
+      percentage: '-25%',
+      title: 'Loja 1',
+      iconColor: '#03C9D7',
+      iconBg: '#E5FAFB',
+      pcColor: 'red-600',
+    },
+    {
+      icon: <BsBoxSeam />,
+      amount: '',
+      percentage: '+23%',
+      title: 'Loja 2',
+      iconColor: 'rgb(255, 244, 229)',
+      iconBg: 'rgb(254, 201, 15)',
+      pcColor: 'green-600',
+    },
+    {
+      icon: <FiBarChart />,
+      amount: '',
+      percentage: '+38%',
+      title: 'Loja 3',
+      iconColor: 'rgb(228, 106, 118)',
+      iconBg: 'rgb(255, 244, 229)',
+
+      pcColor: 'green-600',
+    },
+    {
+      icon: <HiOutlineRefresh />,
+      amount: '',
+      percentage: '-12%',
+      title: 'Perdas',
+      iconColor: 'rgb(0, 194, 146)',
+      iconBg: 'rgb(235, 250, 242)',
+      pcColor: 'red-600',
+    },
+  ]);
 
 
   return (
