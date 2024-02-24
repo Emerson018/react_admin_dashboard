2806b9ab340c296fa9fdca70ac569595bc6934d6
Emerson Viçosa <emersonvicosa@gmail.com>
2024-02-24 01:16:01 -0300

    voltando com a animação pq tava bugado na refatoracao

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -3,63 +3,53 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Lin
 import axios from 'axios';
 import { LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'; // Importe as configurações do eixo principal, se necessário
 import { useStateContext } from '../../ContextProvider';
-
 const LineChart = () => {
   const { currentMode } = useStateContext();
   const [chartData, setChartData] = useState([]);
-  const palette = ['#ca3542', '#27647b', '#849fad', '#aec0c9', '#57575f', '#37afa9'];
-
+  const [chartData2, setChartData2] = useState([]);
+  const [chartData3, setChartData3] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const urls = ['http://127.0.0.1:8000/loja1/', 'http://127.0.0.1:8000/loja2/', 'http://127.0.0.1:8000/loja3/'];
-        const responses = await Promise.all(urls.map(url => axios.get(url)));
-        const sortedData = responses.map(response => response.data.sort((a, b) => new Date(a.data) - new Date(b.data)));
-        setChartData(sortedData);
-        
-        console.log('Dados do banco de dados:', sortedData);
+        const response = await axios.get('http://127.0.0.1:8000/loja1/');
+        const response2 = await axios.get('http://127.0.0.1:8000/loja2/');
+        const response3 = await axios.get('http://127.0.0.1:8000/loja3/');
+        const palette = ['#ca3542', '#27647b', '#849fad', '#aec0c9', '#57575f', '#37afa9'];
+        // Substitua 'sua-api-endpoint-aqui' pelo endpoint correto da sua API
+        setChartData(response.data);
+        setChartData2(response2.data);
+        setChartData3(response3.data);
+        console.log('Dados do banco de dados:', response.data);
       } catch (error) {
         console.error('Erro ao obter dados do banco de dados:', error);
       }
     };
-
     fetchData();  
   }, []);
-
-  const isDataReady = chartData.every(data => data.length > 0);
-
+  const isDataReady = chartData.length > 0 && chartData2.length > 0 && chartData3.length > 0;
   return (
     <>
       {isDataReady && (
-        <ChartComponent
-          id="line-chart"
-          height="420px"
-          primaryXAxis={LinePrimaryXAxis} // Adapte conforme necessário
-          primaryYAxis={LinePrimaryYAxis} // Adapte conforme necessário
-          chartArea={{ border: { width: 0 } }}
-          tooltip={{ enable: true }}
-          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-          palettes={palette}
-          legendSettings={{ background: currentMode === 'Dark' ? '#33373E' : '#fff', textStyle: { color: currentMode === 'Dark' ? '#fff' : '#333' } }}
-        >
-          <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
-          <SeriesCollectionDirective>
-            {chartData.map((data, index) => (
-              <SeriesDirective
-                key={index}
-                dataSource={data}
-                xName='data'
-                yName="porc_variacao_anual"
-                type="Line"
-                name={`Loja ${index + 1}`}
-                marker={{visible: true}}
-              />
-            ))}
-          </SeriesCollectionDirective>
-        </ChartComponent>
-      )}
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis} // Adapte conforme necessário
+      primaryYAxis={LinePrimaryYAxis} // Adapte conforme necessário
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: currentMode === 'Dark' ? '#33373E' : '#fff', textStyle: { color: currentMode === 'Dark' ? '#fff' : '#333' } }}
+
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        <SeriesDirective dataSource={chartData} xName='data' yName="porc_variacao_anual" type="Line" name='Loja 1' />
+        <SeriesDirective dataSource={chartData2} xName='data' yName="porc_variacao_anual" type="Line" name='Loja 2' />
+        <SeriesDirective dataSource={chartData3} xName='data' yName="porc_variacao_anual" type="Line" name='Loja 3'  />
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    )}
     </>
   );
 };
-
 export default LineChart;
--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -4,65 +4,58 @@ import axios from 'axios';
 import { barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
 import { ChartsHeader } from '../../components/Charts';
 import { useStateContext } from '../../ContextProvider';
-
 const Bar = () => {
   const { currentMode } = useStateContext();
   const [barData, setBarData] = useState([]);
+  const [barData2, setBar2Data] = useState([]);
+  const [barData3, setBar3Data] = useState([]);
   const palette = ['#ca3542', '#27647b', '#849fad', '#aec0c9', '#57575f', '#37afa9'];
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const urls = ['http://127.0.0.1:8000/loja1/', 'http://127.0.0.1:8000/loja2/', 'http://127.0.0.1:8000/loja3/'];
-        const responses = await Promise.all(urls.map(url => axios.get(url)));
-        const sortedData = responses.map(response => response.data);
-        setBarData(sortedData);
-        console.log('Dados do banco de dados:', sortedData);
+        const response = await axios.get('http://127.0.0.1:8000/loja1/');
+        const response2 = await axios.get('http://127.0.0.1:8000/loja2/');
+        const response3 = await axios.get('http://127.0.0.1:8000/loja3/');
+        setBarData(response.data);
+        setBar2Data(response2.data);
+        setBar3Data(response3.data);
+        console.log('Dados do banco de dados:', response.data);
       } catch (error) {
         console.error('Erro ao obter dados do banco de dados:', error);
       }
     };
-
     fetchData();  
   }, []);
-
-  const isDataReady = barData.every(data => data.length > 0);
-
+  const isDataReady = barData.length > 0 && barData2.length > 0 && barData3.length > 0;
   return (
     <>
       {isDataReady && (
-        <div className={`chart-layout ${currentMode === 'Dark' ? 'dark:bg-secondary-dark-bg' : ''}`}>
-          <ChartsHeader category="Bar" title="Contagem de Medalhas Olímpicas - RIO" />
-          <div className="w-full">
-            <ChartComponent
-              id="charts"
-              primaryXAxis={barPrimaryXAxis}
-              primaryYAxis={barPrimaryYAxis}
-              chartArea={{ border: { width: 0 } }}
-              tooltip={{ enable: true }}
-              background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-              palettes={palette}
-              legendSettings={{ background: currentMode === 'Dark' ? '#33373E' : '#fff', textStyle: { color: currentMode === 'Dark' ? '#fff' : '#333' } }}
-            >
-              <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
-              <SeriesCollectionDirective>
-                {barData.map((data, index) => (
-                  <SeriesDirective
-                    key={index}
-                    dataSource={data}
-                    xName='tipo_produto'
-                    yName="qtd_produtos"
-                    type="Column"
-                    name={`Loja ${index + 1}`}
-                  />
-                ))}
-              </SeriesCollectionDirective>
-            </ChartComponent>
-          </div>
-        </div>
-      )}
+    <div className="chart-layout dark:bg-secondary-dark-bg">
+      <ChartsHeader category="Bar" title="Contagem de Medalhas Olímpicas - RIO" />
+      <div className=" w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          palettes={palette}
+          legendSettings={{ background: currentMode === 'Dark' ? '#33373E' : '#fff', textStyle: { color: currentMode === 'Dark' ? '#fff' : '#333' } }}
+        >
+          <Inject services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]} />
+          <SeriesCollectionDirective>
+            {/* Utilize o seriesData em vez do barCustomSeries estático */}
+            <SeriesDirective dataSource={barData} xName='tipo_produto' yName="qtd_produtos" type="Column" name='Loja 1' />
+            <SeriesDirective dataSource={barData2} xName='tipo_produto' yName="qtd_produtos" type="Column" name='Loja 2' />
+            <SeriesDirective dataSource={barData3} xName='tipo_produto' yName="qtd_produtos" type="Column" name='Loja 3' />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+    )}
     </>
   );
 };
-
 export default Bar;
