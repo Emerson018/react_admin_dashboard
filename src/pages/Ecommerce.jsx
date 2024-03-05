5d779cfe80e98a09ac897afa513cda40d039dba3
Emerson Viçosa <emersonvicosa@gmail.com>
2024-03-04 23:39:07 -0300

    colocando fundo no layouts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -131,123 +131,125 @@ const Ecommerce = () => {
   }, [redirect]);
 
   return (
-    <div className='mt-12'>
-      <div className='chart-ecommerce-layout'>
-        <div className='body-budget-card dark:bg-secondary-dark-bg'>
-          <div className='chart-ecommerce-layout'>
-            <div>
-              <p className='budget-element'>Rendimento Total</p>
-              <p className='total-budget'>
-                R$ {rendimentoTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
-            </div>
-          </div>
-          <div className='ecommerce-button'>
-            <Button
-              color='white'
-              bgColor={currentColor}
-              text='Download'
-              borderRadius='10px'
-              size='md'
-            />
-          </div>
-        </div>
-
-        <div className='store-elements'>
-        {earningDataWithBackend.map((item) => (
-            <div
-              key={item.title}
-              className='store-layout dark:text-gray-200 dark:bg-secondary-dark-bg'
-            >
-              <button 
-                type='button'
-                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className='button-store-elements'
-              >
-              </button>
-              <p className='store-scale'>
-                <span className='text-store-layout'>R$ 
-                  {item.earning.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-                </span>
-                <span className={`percent-txt text-${item.pcColor}`}>
-                  {item.percentage}
-                </span>
-              </p>
-              <p className='text-store-secondary'>
-                {item.title}
-                </p>
-                <p className='text-store-secondary'>
-              Baixas: R$ {item.losses.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-            </p>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className='container-revenue'>
-        <div className='container-revenue-layout dark:text-gray-200 dark:bg-secondary-dark-bg'>
-          <div className='container-revenue-top-left'>
-            <p className='container-revenue-title'>Atualizações das lojas</p>
-              <div className='container-revenue-text'>
-                <p className='container-revenue-text
-                  text-gray-600
-                  hover:drop-shadow-x1'>
-                  <span><GoDotFill  /></span>
-                  <span>Expense</span>
-                </p>
-                <p className='container-revenue-text
-                container-revenue-text-budget'>
-                  <span><GoDotFill  /></span>
-                  <span>Gráfico</span>
-                </p>
-              </div>
-          </div>
-          <div className='container-stacked-graph'>
-            <div className='container-stacked-layout'>
+    <div className="chart-layout dark:bg-secondary-dark-bg">
+      <div className='mt-12'>
+        <div className='chart-ecommerce-layout'>
+          <div className='body-budget-card dark:bg-secondary-dark-bg'>
+            <div className='chart-ecommerce-layout'>
               <div>
-                <p>
-                  <span className='container-customers'>{clientesTotais}</span>
-                  <span className='container-float-value'>23%</span>
-                </p>
-                <p className='text-gray-500'>Clientes Totais</p>
-              </div>
-              <div  className='mt-8'>
-                <p>
-                  <span className='text-3xl font-semibold'>R$ {maiorVenda.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-                </p>
-                <p className='text-gray-500'>Maior Valor de Vendas</p>
-              </div>
-
-              <div className='mt-5'>
-                <SparkLine
-                  curremtColor={currentColor}
-                  id='line-sparkline'
-                  type='line'
-                  height='80px'
-                  width='250px'
-                  data={SparklineAreaData}
-                  color={currentColor}
-                />
-              </div>
-              <div className='mt-10'>
-                <Button
-                  color='white'
-                  bgColor={currentColor}
-                  text='Download'
-                  borderRadius='10px'
-                />
-
+                <p className='budget-element'>Rendimento Total</p>
+                <p className='total-budget'>
+                  R$ {rendimentoTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
               </div>
             </div>
-            <div>
-              <Stacked 
-                width="320px"
-                height="360px" 
+            <div className='ecommerce-button'>
+              <Button
+                color='white'
+                bgColor={currentColor}
+                text='Download'
+                borderRadius='10px'
+                size='md'
               />
             </div>
           </div>
+
+          <div className='store-elements'>
+          {earningDataWithBackend.map((item) => (
+              <div
+                key={item.title}
+                className='store-layout dark:text-gray-200 dark:bg-secondary-dark-bg'
+              >
+                <button 
+                  type='button'
+                  style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                  className='button-store-elements'
+                >
+                </button>
+                <p className='store-scale'>
+                  <span className='text-store-layout'>R$ 
+                    {item.earning.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                  </span>
+                  <span className={`percent-txt text-${item.pcColor}`}>
+                    {item.percentage}
+                  </span>
+                </p>
+                <p className='text-store-secondary'>
+                  {item.title}
+                  </p>
+                  <p className='text-store-secondary'>
+                Baixas: R$ {item.losses.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+              </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className='container-revenue'>
+          <div className='container-revenue-layout dark:text-gray-200 dark:bg-secondary-dark-bg'>
+            <div className='container-revenue-top-left'>
+              <p className='container-revenue-title'>Atualizações das lojas</p>
+                <div className='container-revenue-text'>
+                  <p className='container-revenue-text
+                    text-gray-600
+                    hover:drop-shadow-x1'>
+                    <span><GoDotFill  /></span>
+                    <span>Expense</span>
+                  </p>
+                  <p className='container-revenue-text
+                  container-revenue-text-budget'>
+                    <span><GoDotFill  /></span>
+                    <span>Gráfico</span>
+                  </p>
+                </div>
+            </div>
+            <div className='container-stacked-graph'>
+              <div className='container-stacked-layout'>
+                <div>
+                  <p>
+                    <span className='container-customers'>{clientesTotais}</span>
+                    <span className='container-float-value'>23%</span>
+                  </p>
+                  <p className='text-gray-500'>Clientes Totais</p>
+                </div>
+                <div  className='mt-8'>
+                  <p>
+                    <span className='text-3xl font-semibold'>R$ {maiorVenda.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                  </p>
+                  <p className='text-gray-500'>Maior Valor de Vendas</p>
+                </div>
+
+                <div className='mt-5'>
+                  <SparkLine
+                    curremtColor={currentColor}
+                    id='line-sparkline'
+                    type='line'
+                    height='80px'
+                    width='250px'
+                    data={SparklineAreaData}
+                    color={currentColor}
+                  />
+                </div>
+                <div className='mt-10'>
+                  <Button
+                    color='white'
+                    bgColor={currentColor}
+                    text='Download'
+                    borderRadius='10px'
+                  />
+
+                </div>
+              </div>
+              <div>
+                <Stacked 
+                  width="320px"
+                  height="360px" 
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
